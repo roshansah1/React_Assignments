d75dbaef18dedcd98803cf779db0e52a8b14ccf0
roshansah1 <Roshansah9909@gmail.com>
2023-01-26 14:50:22 +0530

    Updates

---
+++ assignment-1/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import About from "./About";
+
+
+const App = () => {
+  return(
+    <>
+       <About />
+    </>
+  )
+}
+
+
+export default App;
---
+++ assignment-2/src/Home.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Home = () => {
+    return(
+        <>
+         <div className="main_div">
+             <div className="content">
+             <h1> The Evolution of </h1>
+             <h1> Airbnb's Frontend</h1>
+             </div>
+             <div className="footer">
+                <p> @spikebrehm </p>
+                <div className="logo">
+                <i class="fa-brands fa-airbnb"></i>
+                <p className="small_font"> airbnb </p>
+                </div>
+             </div>
+         </div>
+        </>
+    )
+}
+
+export default Home;
